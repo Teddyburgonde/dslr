2971b835f650b640eac40f6578685487b3fba5a4
Teddyburgonde <bandama.teddy@gmail.com>
2026-02-02 22:49:37 +0100

    modified:   training/training.py Added std function (standard deviation)

--- training/training.py
+++ training/training.py
@@ -1,11 +1,10 @@
 import sys
 
-"""
-- nan -> C'est une valeur vide dans le tableau.
-- La fonction count est important car elle permet de calculer toutes les autres statistiques.
-"""
+
 def count(arr):
-	"""Compte le nombre de valeurs non-NaN"""
+	"""Compte le nombre de valeurs non-NaN
+	- nan -> C'est une valeur vide dans le tableau.
+	- La fonction count est important car elle permet de calculer toutes les autres statistiques."""
 	counter = 0
 	for x in arr:
 		# NaN est la seule valeur qui n'est pas égale à elle-même
@@ -18,7 +17,10 @@ def count(arr):
 moyenne = Somme des valeurs / nombre total
 """
 def mean(arr):
-	"""Calcule la moyenne en ignorant les NaN"""
+	"""Calcule la moyenne en ignorant les NaN.
+	μ = Σx / n
+	moyenne = Somme des valeurs / nombre total
+	"""
 	somme = 0
 	counter = count(arr)
 
@@ -31,9 +33,29 @@ def mean(arr):
 	moyenne = somme / counter
 	return moyenne
 
-# def std(arr):
-# 	"""Calcule l'écart-type (population)"""
 
+def std(arr):
+	"""Calcule l'écart-type (population).
+	écart-type = racine carrée de la moyenne des carrés des écarts"""
+	# Calcule de la moyenne
+	mean_value = mean(arr)
+	ecarts = []
+	carres = []
+	for x in arr:
+		if x == x:
+			# Calcule l'ecarte de chaque valeur par rapport à la moyenne (valeaur - moyenne)
+			ecart = x - mean_value
+			ecarts.append(ecart)
+			
+			# Éleve chaque écart au carré
+			carre = ecart * ecart
+			carres.append(carre)
+
+	# Calcule de la moyenne de ces carrés puis prends la racine carrée
+	mean_carre = mean(carres)
+	racine = mean_carre ** 0.5
+
+	return racine
 
 
 
