ef6714604e353944c6db5d843a691cbd944b1411
Teddyburgonde <bandama.teddy@gmail.com>
2026-02-02 20:01:38 +0100

    modified:   training/training.py Added mean function

--- training/training.py
+++ training/training.py
@@ -13,6 +13,31 @@ def count(arr):
 			counter+=1
 	return counter
 
+"""
+μ = Σx / n
+moyenne = Somme des valeurs / nombre total
+"""
+def mean(arr):
+	"""Calcule la moyenne en ignorant les NaN"""
+	somme = 0
+	counter = count(arr)
+
+	for x in arr:
+		if x == x:
+			somme += x
+	if counter == 0: # Evite les divisions par 0
+		return float('nan')
+	
+	moyenne = somme / counter
+	return moyenne
+
+# def std(arr):
+# 	"""Calcule l'écart-type (population)"""
+
+
+
+
+
 def main():
     print(count([1, 2, 'nan' ,4]))
 
